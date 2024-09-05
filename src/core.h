c0ae3b5bb9d0bf7a1daecfeff7d69446b524cc99
Campanita <rapunzel@disroot.org>
2024-09-04 19:10:26 -0600

    Temp

---
+++ src/core.h
@@ -0,0 +1,15 @@
+/**
+ * @file core.h
+ * Define la estructura del microprocesador virtual
+ */
+
+/**
+ * @class Lily
+ */
+struct Lily {
+  /*
+  Z80: 8d, 16a
+  80386:32d, 32a
+  HC11: 8d, 16a
+  */
+};
