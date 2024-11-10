ab2f3a5e86447ec56e51fceaf7c8fc4cd2ff9628
Giovanni Alfredo Garciliano Díaz <rapunzel@disroot.org>
2024-11-10 02:17:50 -0600

    Implementación de diccionario

---
+++ src/common/dict.c
@@ -0,0 +1,102 @@
+#include "dict.h"
+
+struct Dict_Dict* Dict_Create(void){
+    struct Dict_Dict* obj = (struct Dict_Dict*) calloc(1, sizeof(struct Dict_Dict));
+    obj->raiz = NULL;
+    obj->tamano = 0;
+    return obj;
+}
+
+struct Dict_Nodo* Dict_Nodo_Create(void){
+    struct Dict_Nodo* obj = (struct Dict_Nodo*) calloc(1, sizeof(struct Dict_Nodo));
+    obj->padre = NULL;
+    obj->izquierda = NULL;
+    obj->derecha = NULL;
+    obj->clave = NULL;
+    obj->valor = NULL;
+    return obj;
+}
+
+void Dict_IntercambiarNodos(struct Dict_Dict* dict, struct Dict_Nodo* u, struct Dict_Nodo* v){
+    if (u->padre == NULL) dict->raiz = v;
+    else if (u == u->padre->izquierda) u->padre->izquierda = v;
+    else u->padre->derecha = v;
+    if (v != NULL) v->padre = u->padre;
+    return;
+}
+
+struct Dict_Nodo* Dict_Insert(struct Dict_Dict* dict, const char* clave, const void* valor){
+    int comp;
+    struct Dict_Nodo* padre = NULL;
+    struct Dict_Nodo* nodo = dict->raiz;
+    while (nodo != NULL){
+        padre = nodo;
+        comp = strcmp(clave, nodo->clave);
+        if (comp < 0) nodo = nodo->izquierda;
+        else if (comp > 0) nodo = nodo->derecha;
+        else return NULL; // Clave es igual
+    }
+    struct Dict_Nodo* nuevo = Dict_Nodo_Create();
+    nuevo->padre = padre;
+    nuevo->clave = clave;
+    nuevo->valor = valor;
+    if (padre == NULL) dict->raiz = nuevo;
+    else {
+        comp = strcmp(clave, padre->clave);
+        if (comp < 0) padre->izquierda = nuevo;
+        else if (comp > 0) padre->derecha = nuevo;
+    }
+    dict->tamano++;
+    return nuevo;
+}
+
+struct Dict_Nodo* Dict_Get(struct Dict_Dict* dict, const char* clave){
+    struct Dict_Nodo* nodo = dict->raiz;
+    while (nodo != NULL && strcmp(clave, nodo->clave)){
+        int comp = strcmp(clave, nodo->clave);
+        if (comp < 0) nodo = nodo->izquierda;
+        else if (comp > 0) nodo = nodo->derecha;
+    }
+    return nodo;
+}
+
+void Dict_Remove(struct Dict_Dict* dict, const char* clave){
+    struct Dict_Nodo* nodo = Dict_Get(dict, clave);
+    if (nodo->izquierda == NULL) Dict_IntercambiarNodos(dict, nodo, nodo->derecha);
+    else if (nodo->derecha == NULL) Dict_IntercambiarNodos(dict, nodo, nodo->izquierda);
+    else {
+        // Sucesor
+        struct Dict_Nodo* sucesor;
+        struct Dict_Nodo* nodo_temp = nodo;
+        if (nodo_temp->derecha != NULL){
+            // Minimo
+            struct Dict_Nodo* nodo_temp2 = nodo_temp->derecha;
+            while (nodo_temp2->izquierda != NULL){
+                nodo_temp2 = nodo_temp2->izquierda;
+            }
+            sucesor = nodo_temp2;
+            // End Minimo
+        } else {
+            sucesor = nodo_temp->padre;
+            while (sucesor != NULL && nodo_temp == sucesor->derecha){
+                nodo_temp = sucesor;
+                sucesor = sucesor->padre;
+            }
+        }
+        // End Sucesor
+        if (sucesor->padre != nodo){
+            Dict_IntercambiarNodos(dict, sucesor, sucesor->derecha);
+            sucesor->derecha = nodo->derecha;
+            sucesor->derecha->padre = sucesor;
+        }
+        Dict_IntercambiarNodos(dict, nodo, sucesor);
+        sucesor->izquierda = nodo->izquierda;
+        sucesor->izquierda->padre = sucesor;
+    }
+    dict->tamano--;
+    return;
+}
+
+size_t Dict_Size(struct Dict_Dict* dict){
+    return dict->tamano;
+}
---
+++ src/common/dict.h
@@ -0,0 +1,69 @@
+/**
+ * @file dict.h
+ * Funciones para implementar un diccionario
+ */
+
+#ifndef LILY_C_DICT
+#define LILY_C_DICT
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+struct Dict_Nodo {
+    struct Dict_Nodo* padre;
+    struct Dict_Nodo* izquierda;
+    struct Dict_Nodo* derecha;
+    char* clave;
+    void* valor;
+};
+
+struct Dict_Dict {
+    struct Dict_Nodo* raiz;
+    size_t tamano;
+};
+
+/**
+ * Crea un diccionario vacío
+ * @return Puntero a un diccionario
+ */
+struct Dict_Dict* Dict_Create(void);
+
+/**
+ * Crea un nodo de diccionario vacío
+ * @return Puntero a un nodo de diccionario
+ */
+struct Dict_Nodo* Dict_Nodo_Create(void);
+
+/**
+ * Añade una clave al diccionario
+ * @param dict Diccionario a manipular
+ * @param clave Clave a añadir
+ * @param valor Valor a añadir
+ * @return Nodo recién insertado
+ */
+struct Dict_Nodo* Dict_Insert(struct Dict_Dict* dict, const char* clave, const void* valor);
+
+/**
+ * Devuelve el valor de una clave del diccionario
+ * @param dict Diccionario a manipular
+ * @param clave Clave a devolver
+ * @return Nodo que contiene la clave
+ */
+struct Dict_Nodo* Dict_Get(struct Dict_Dict* dict, const char* clave);
+
+/**
+ * Quita una clave del diccionario
+ * @param dict Diccionario a manipular
+ * @param clave Clave a eliminar
+ */
+void Dict_Remove(struct Dict_Dict* dict, const char* clave);
+
+/**
+ * Devuelve el número de elementos del diccionario
+ * @param dict Diccionario a manipular
+ * @return Número de elementos del diccionario
+ */
+size_t Dict_Size(struct Dict_Dict* dict);
+
+#endif
