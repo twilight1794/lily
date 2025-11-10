24d394c364d07d4d06c41ca5de7c90b64c10582f
campanita (nina) <rapunzel@disroot.org>
2025-11-10 16:17:45 -0600

    Añad. registros del 8086

---
+++ misc/8086.lua
@@ -0,0 +1,135 @@
+-- Especificación del microprocesador Intel 8086
+
+return {
+    id = "intel.8086"
+    registros = {
+        AL = {
+            tamano = 8,
+            desplazamiento = 0
+        },
+        AH = {
+            tamano = 8,
+            desplazamiento = 8
+        },
+        AX = {
+            tamano = 16,
+            desplazamiento = 0
+        },
+        CL = {
+            tamano = 8,
+            desplazamiento = 16
+        },
+        CH = {
+            tamano = 8,
+            desplazamiento = 24
+        },
+        CX = {
+            tamano = 16,
+            desplazamiento = 16
+        },
+        DL = {
+            tamano = 8,
+            desplazamiento = 32
+        },
+        DH = {
+            tamano = 8,
+            desplazamiento = 40
+        },
+        DX = {
+            tamano = 16,
+            desplazamiento = 32
+        },
+        BL = {
+            tamano = 8,
+            desplazamiento = 48
+        },
+        BH = {
+            tamano = 8,
+            desplazamiento = 56
+        },
+        BX = {
+            tamano = 16,
+            desplazamiento = 48
+        },
+        SP = {
+            tamano = 16,
+            desplazamiento = 64
+        },
+        BP = {
+            tamano = 16,
+            desplazamiento = 80
+        },
+        SI = {
+            tamano = 16,
+            desplazamiento = 96
+        },
+        DI = {
+            tamano = 16,
+            desplazamiento = 112
+        },
+        IP = {
+            tamano = 16,
+            desplazamiento = 128
+        },
+        ES = {
+            tamano = 16,
+            desplazamiento = 144
+        },
+        CS = {
+            tamano = 16,
+            desplazamiento = 160
+        },
+        SS = {
+            tamano = 16,
+            desplazamiento = 176
+        },
+        DS = {
+            tamano = 16,
+            desplazamiento = 192
+        },
+        CF = {
+            tamano = 1,
+            desplazamiento = 208
+        },
+        PF = {
+            tamano = 1,
+            desplazamiento = 209
+        },
+        AF = {
+            tamano = 1,
+            desplazamiento = 210
+        },
+        ZF = {
+            tamano = 1,
+            desplazamiento = 211
+        },
+        SF = {
+            tamano = 1,
+            desplazamiento = 212
+        },
+        TF = {
+            tamano = 1,
+            desplazamiento = 213
+        },
+        IF = {
+            tamano = 1,
+            desplazamiento = 214
+        },
+        DF = {
+            tamano = 1,
+            desplazamiento = 215
+        },
+        OF = {
+            tamano = 1,
+            desplazamiento = 216
+        },
+    },
+    grupos = {
+    },
+    ensamble = {
+    },
+    opcodes = {
+    },
+    desensamble = {
+    }
+}
