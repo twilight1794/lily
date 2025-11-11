47479b4ade0e2d454ff5db5fd48983357175d2e2
campanita (nina) <rapunzel@disroot.org>
2025-11-10 19:07:56 -0600

    Añad. mnemónicos a describir

--- misc/8086.lua
+++ misc/8086.lua
@@ -127,6 +127,242 @@ return {
     grupos = {
     },
     ensamble = {
+        AAA = {
+        },
+        AAD = {
+        },
+        AAM = {
+        },
+        AAS = {
+        },
+        ADC = {
+        },
+        ADD = {
+        },
+        AND = {
+        },
+        CALL = {
+        },
+        CBW = {
+        },
+        CLC = {
+        },
+        CLD = {
+        },
+        CLI = {
+        },
+        CMC = {
+        },
+        CMP = {
+        },
+        CMPSB = {
+        },
+        CMPSW = {
+        },
+        CWD = {
+        },
+        DAA = {
+        },
+        DAS = {
+        },
+        DEC = {
+        },
+        DIV = {
+        },
+        ESC = {
+        },
+        HLT = {
+        },
+        IDIV = {
+        },
+        IMUL = {
+        },
+        IN = {
+        },
+        INC = {
+        },
+        INT = {
+        },
+        INTO = {
+        },
+        IRET = {
+        },
+        JA = {
+        },
+        JAE = {
+        },
+        JB = {
+        },
+        JBE = {
+        },
+        JC = {
+        },
+        JE = {
+        },
+        JG = {
+        },
+        JGE = {
+        },
+        JL = {
+        },
+        JLE = {
+        },
+        JNA = {
+        },
+        JNAE = {
+        },
+        JNB = {
+        },
+        JNBE = {
+        },
+        JNC = {
+        },
+        JNE = {
+        },
+        JNG = {
+        },
+        JNGE = {
+        },
+        JNL = {
+        },
+        JNLE = {
+        },
+        JNO = {
+        },
+        JNP = {
+        },
+        JNS = {
+        },
+        JNZ = {
+        },
+        JO = {
+        },
+        JP = {
+        },
+        JPE = {
+        },
+        JPO = {
+        },
+        JS = {
+        },
+        JZ = {
+        },
+        JCXZ = {
+        },
+        JMP = {
+        },
+        LAHF = {
+        },
+        LDS = {
+        },
+        LEA = {
+        },
+        LES = {
+        },
+        LOCK = {
+        },
+        LODSB = {
+        },
+        LODSW = {
+        },
+        LOOP = {
+        },
+        LOOPE = {
+        },
+        LOOPNE = {
+        },
+        LOOPNZ = {
+        },
+        LOOPZ = {
+        },
+        MOV = {
+        },
+        MOVSB = {
+        },
+        MOVSW = {
+        },
+        MUL = {
+        },
+        NEG = {
+        },
+        NOP = {
+        },
+        NOT = {
+        },
+        OR = {
+        },
+        OUT = {
+        },
+        POP = {
+        },
+        POPF = {
+        },
+        PUSH = {
+        },
+        PUSHF = {
+        },
+        RCL = {
+        },
+        RCR = {
+        },
+        REP = {
+        },
+        REPE = {
+        },
+        REPNE = {
+        },
+        REPNZ = {
+        },
+        REPZ = {
+        },
+        RET = {
+        },
+        RETN = {
+        },
+        RETF = {
+        },
+        ROL = {
+        },
+        ROR = {
+        },
+        SAHF = {
+        },
+        SAL = {
+        },
+        SAR = {
+        },
+        SBB = {
+        },
+        SCASB = {
+        },
+        SCASW = {
+        },
+        SHL = {
+        },
+        SHR = {
+        },
+        STC = {
+        },
+        STD = {
+        },
+        STI = {
+        },
+        STOSB = {
+        },
+        STOSW = {
+        },
+        SUB = {
+        },
+        TEST = {
+        },
+        WAIT = {
+        },
+        XCHG = {
+        },
+        XLAT = {
+        },
+        XOR = {
+        },
     },
     opcodes = {
     },
