a18a3d15b411fd9237b0bd70f8ec0e1243ac698b
Campanita (tornasol) <rapunzel@disroot.org>
2026-01-13 16:48:04 -0600

    main: definidas opciones de consola

--- src/cli/main.c
+++ src/cli/main.c
@@ -27,27 +27,55 @@
 #define LILY_MODIFICADO ""
 #endif
 
-void f_help(char* name){
+void f_help(char* name) {
     printf(_("Usage: %s [<params>] <file>\n\n"), name);
-    puts(_("Options:"));
-    /* Misc */
-    puts(_(" -h               Show this help"));
-    puts(_(" -v               Show Lily version"));
-    /* Preprocess stage options */
-    puts(_(" -L<path>         Generate listing file in <path>"));
-    puts(_(" -D<tag>=<value>  Define a tag <tag> with a value <value>"));
-    puts(_(" -I<path>         Search for assembly files in <path>"));
+    puts(_("Key: P (preprocessor), A (assembly),"));
+    puts(_("     L (linking), D (disassembly), E (execution)"));
+    /* Opciones del preprocesador */
+    puts(_("\nPreprocessor options:"));
+    puts(_(" -L<path>           --listing=<path>          Generate listing file in <path>"));
+    puts(_(" -D<tag>[=<value>]  --define=<tag>[=<value>]  Define a tag <tag> with a value <value>"));
+    puts(_(" -I<path>           --include=<path>          Search for assembly files first in <path>"));
+    /* Opciones de ensamble */
+    /* Opciones de enlazado */
+    /* Opciones de desensamble */
+    /* Opciones de ejecución */
+    puts(_("\nExecution options:"));
+    puts(_(" -i  --interactive  Show a simple CLI that shows the state of the hypervisor"));
+    //puts(_(" -m=<mapping>  --mem-mapping=<mapping>  Map a memory address with a device"));
+    //puts(_(" -s=<file>     --state=<file>           "));
+    //puts(_(" -S            --step                   Execute a single mnemonic and exit, saving state."));
+    /* Opciones para varias etapas */
+    puts(_("\nMultistage options:"));
+    puts(_(" -o<file>              --output=<file>                Output file (all)"));
+    puts(_(" -x<arch>              --architecture=<arch-or-file>  Working architecture (all except L)"));
+    puts(_(" -W<warning>           --warning=<warning>            Enables warning <warning> (all)"));
+    puts(_(" -Wno<warning>         --warning=no<warning>          Disables warning <warning> (all)"));
+    puts(_(" -E<error>             --error=<error>                Enables error <error> (all)"));
+    puts(_(" -Eno<error>           --error=no<error>              Disables error <error> (all)"));
+    puts(_(" -O<option>[=<value>]  --option=<option>[=<value>]    Passes option <option> to stage (all)"));
+    puts(_(" -p                    --pedantic                     Enables all warnings (all)"));
+    puts(_(" -P                    --superpedantic                Enables all warnings and treat them as errors (all)"));
+    puts(_(" -f<format>            --input-format=<format>        Format of input object (D, E)"));
+    puts(_(" -F<format>            --output-format=<format>       Format of output object (L)"));
+    /* Miscelánea */
+    puts(_("\nMiscellaneous options:"));
+    puts(_(" -s<stage>   --stage=<stage>    Operation to perform: P, A, L, D, or E"));
+    puts(_(" -l=<level>  --logging=<level>  Restrict output to <level>"));
+    puts(_(" -h          --help             Show this help")); //NOTE=class[,…]
+    puts(_(" -v          --version          Show Lily version"));
     puts(_("\nFor more info, see the man pages."));
-    /* Assembly stage options */
-    puts(_(" -A<path>         Save file generated in assemble stage in <path>"));
 }
 
 void f_version(void){
     printf(_("Lily %s (commit %s%s)\n"), LILY_VERSION, LILY_COMMIT, LILY_MODIFICADO);
-    puts(_("Copyright (C) 2024 Giovanni Alfredo Garciliano Diaz"));
+    puts(_("Copyright (C) 2024-2026 Giovanni Alfredo Garciliano Diaz"));
     puts(_("License GNU GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>"));
     puts(_("This is free software: you are free to change and redistribute it"));
     puts(_("There is NO WARRANTY, to the extent permitted by law."));
+
+    puts(_("Report bugs to: <hola@campanita.xyz>"));
+    puts(_("home page: <https://github.com/twilight1794/lily/>"));
 }
 
 int main(int argc, char **argv){
