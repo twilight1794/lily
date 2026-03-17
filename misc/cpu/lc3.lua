13ccbf14934d14ac314e139d684fe5e9683eb6cf
Campanita (midge) <rapunzel@disroot.org>
2026-03-16 23:18:02 -0600

    misc: corr. identación de lc3.lua

--- misc/cpu/lc3.lua
+++ misc/cpu/lc3.lua
@@ -72,104 +72,104 @@ return {
         ind6 = function(desp) return #desp == 2 and type(desp[1]) == "string" and desp[1]:sub(1, 1):lower() == "r" and (tonumber(desp[1]:sub(2, 2)) >= 0 and tonumber(desp[1]:sub(2, 2)) < 8) and type(desp[2]) == "number" and desp[2] >= -32 and desp[2] < 32 end,
     },
     ensamble = {
-          -- Instrucciones de operación
-          ADD = {
-              {
-                  { "r", "r", "r" },
-                  function(rd, r1, r2)
-                      return {
-                          0x10 | (obt_r(rd) << 1) | ((obt_r(r1) & 4) >> 2),
-                          ((obt_r(r1) & 3) << 6) | obt_r(r2)
-                      }
-                  end
-              },
-              {
-                  { "r", "r", "uint5" },
-                  function (rd, r1, n)
-                      return {
-                          0x10 | (obt_r(rd) << 1) | ((obt_r(r1) & 4) >> 2),
-                          0x20 | ((obt_r(r1) & 3) << 6) | n
-                      }
-                  end
-              }
-          },
-          AND = {
-              {
-                  { "r", "r", "r" },
-                  { "ADD", function(bytes) return { bytes[1] + 0x40, bytes[2] } end }
-              },
-              {
-                  { "r", "r", "uint5" },
-                  { "ADD", function(bytes) return { bytes[1] + 0x40, bytes[2] } end }
-              }
-          },
-          NOT = {
-              {
-                  { "r", "r" },
-                  function(r1, r2)
-                      return {
-                          0x90 | (obt_r(r1) << 1) | ((obt_r(r2) & 4) >> 2),
-                          0x3F | ((obt_r(r2) & 3) << 6)
-                      }
-                  end,
-              }
-          },
-          -- Instrucciones de movimiento
-          LD = {
-              {
-                  { "r", "dir9" },
-                  function(r, v) return { 0x20 | (obt_r(r) << 1) | ((v & 0x100) >> 8), v & 0xFF } end
-              },
-              {
-                  { "r", "ind9" },
-                  function(r, v) return { 0xA0 | (obt_r(r) << 1) | ((v[1] & 0x100) >> 8), v[1] & 0xFF } end
-              },
-              {
-                  { "r", "ind6" },
-                  function(r, v) return { 0x60 | obt_r(r) << 1 |((obt_r(v[1]) & 4) >> 2), ((obt_r(v[1]) & 3) << 6) | v[2] } end
-              }
-          },
-          LEA = {
-              {
-                  { "r", "dir9" },
-                  function(r, v) return { 0x70 | (obt_r(r) << 1) | ((v & 0x100) >> 8), v & 0xFF } end
-              }
-          },
-          ST = {
-              {
-                  { "r", "dir9" },
-                  { "LD", function(bytes) return { bytes[1] + 0x10, bytes[2] } end }
-              },
-              {
-                  { "r", "ind9" },
-                  { "LD", function(bytes) return { bytes[1] + 0x10, bytes[2] } end }
-              },
-              {
-                  { "r", "ind6" },
-                  { "LD", function(bytes) return { bytes[1] + 0x10, bytes[2] } end }
-              }
-          },
-          -- Instrucciones de control
-          BR = {
-              {
-                  { "uint3", "dir9" },
-                  function (f, v) return { (f << 1) | ((v & 0x100) >> 8), v & 0xFF } end
-              }
-          },
-          JMP = {
-              {
-                  { "r" },
-                  function (r) return { 0xC0 | ((obt_r(r) & 4) >> 2), ((obt_r(r) & 3) << 6) } end
-              }
-          },
-          JSR = nil,
-          JSRR = nil,
-          RET = { 0xC1, 0xC0 },
-          TRAP = {
-              {
-                  { "uint8" },
-                  function (v) return { 0xF0, v } end
-              }
+        -- Instrucciones de operación
+        ADD = {
+            {
+                { "r", "r", "r" },
+                function(rd, r1, r2)
+                    return {
+                        0x10 | (obt_r(rd) << 1) | ((obt_r(r1) & 4) >> 2),
+                        ((obt_r(r1) & 3) << 6) | obt_r(r2)
+                    }
+                end
+            },
+            {
+                { "r", "r", "uint5" },
+                function (rd, r1, n)
+                    return {
+                        0x10 | (obt_r(rd) << 1) | ((obt_r(r1) & 4) >> 2),
+                        0x20 | ((obt_r(r1) & 3) << 6) | n
+                    }
+                end
+            }
+        },
+        AND = {
+            {
+                { "r", "r", "r" },
+                { "ADD", function(bytes) return { bytes[1] + 0x40, bytes[2] } end }
+            },
+            {
+                { "r", "r", "uint5" },
+                { "ADD", function(bytes) return { bytes[1] + 0x40, bytes[2] } end }
+            }
+        },
+        NOT = {
+            {
+                { "r", "r" },
+                function(r1, r2)
+                    return {
+                        0x90 | (obt_r(r1) << 1) | ((obt_r(r2) & 4) >> 2),
+                        0x3F | ((obt_r(r2) & 3) << 6)
+                    }
+                end,
+            }
+        },
+        -- Instrucciones de movimiento
+        LD = {
+            {
+                { "r", "dir9" },
+                function(r, v) return { 0x20 | (obt_r(r) << 1) | ((v & 0x100) >> 8), v & 0xFF } end
+            },
+            {
+                { "r", "ind9" },
+                function(r, v) return { 0xA0 | (obt_r(r) << 1) | ((v[1] & 0x100) >> 8), v[1] & 0xFF } end
+            },
+            {
+                { "r", "ind6" },
+                function(r, v) return { 0x60 | obt_r(r) << 1 |((obt_r(v[1]) & 4) >> 2), ((obt_r(v[1]) & 3) << 6) | v[2] } end
+            }
+        },
+        LEA = {
+            {
+                { "r", "dir9" },
+                function(r, v) return { 0x70 | (obt_r(r) << 1) | ((v & 0x100) >> 8), v & 0xFF } end
+            }
+        },
+        ST = {
+            {
+                { "r", "dir9" },
+                { "LD", function(bytes) return { bytes[1] + 0x10, bytes[2] } end }
+            },
+            {
+                { "r", "ind9" },
+                { "LD", function(bytes) return { bytes[1] + 0x10, bytes[2] } end }
+            },
+            {
+                { "r", "ind6" },
+                { "LD", function(bytes) return { bytes[1] + 0x10, bytes[2] } end }
+            }
+        },
+        -- Instrucciones de control
+        BR = {
+            {
+                { "uint3", "dir9" },
+                function (f, v) return { (f << 1) | ((v & 0x100) >> 8), v & 0xFF } end
+            }
+        },
+        JMP = {
+            {
+                { "r" },
+                function (r) return { 0xC0 | ((obt_r(r) & 4) >> 2), ((obt_r(r) & 3) << 6) } end
+            }
+        },
+        JSR = nil,
+        JSRR = nil,
+        RET = { 0xC1, 0xC0 },
+        TRAP = {
+            {
+                { "uint8" },
+                function (v) return { 0xF0, v } end
+            }
         }
     },
     opcodes = {
