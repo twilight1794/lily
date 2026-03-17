039bee8e677512a9e688e3c3418dbf3f087a1c55
Campanita (midge) <rapunzel@disroot.org>
2026-03-16 23:07:34 -0600

    nums: Añad. función de decodificación

--- src/common/nums.c
+++ src/common/nums.c
@@ -5,7 +5,7 @@ enum lily_estado lily_nums_codificar_num(uint8_t* buf, void* val, const size_t t
     union lily_simbolo_numero num;
     num.positivo = *((uint64_t*) val);
 
-    // Validar tamaño
+    // Validar signo y tamaño
     if (signo == 0) {
         // unsigned
         if (tam == 1 && num.positivo > UINT8_MAX) return COD_NUMS_MAS_8;
@@ -28,6 +28,18 @@ enum lily_estado lily_nums_codificar_num(uint8_t* buf, void* val, const size_t t
     // Escribir número
     for (size_t i = 0; i < tam; i++) {
         buf[endianness ? i : (tam-1)-i] = num.bytes[i];
+    }
+    return COD_OK;
+}
+
+enum lily_estado lily_nums_decodificar_num(union lily_simbolo_numero* val, uint8_t* bytes, const size_t tam, const int signo, const bool endianness) {
+    for (size_t i = 0; i < 8; i++) {
+        size_t real_i = endianness ? i : (tam-1)-i;
+        if (i < tam) val->bytes[i] = *(bytes + real_i);
+        else {
+            if (signo == 0 || (signo == 2 && val->negativo >= 0)) val->bytes[i] = 0;
+            else val->bytes[i] = 0xFF;
+        }
     }
     return COD_OK;
 }
